520e6a08bbff1e6407d9f2396daeaacbdde9b909
kilobyte1 <p2562416@my365.dmu.ac.uk>
2024-08-26 23:24:09 +0100

    Rendered components in the App component. Added RoomJoinPage and CreateRoomPage. used react routing to access different pages

---
+++ Tutorial/music_collection/frontend/src/components/HomePage.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+
+import {BrowserRouter as Router, Switch, Route, Link, Redirect,} from "react-router-dom";
+
+export default class HomePage extends Component {
+    constructor (props){
+        super(props);
+    }
+
+    render(){
+        return <Router>
+            <Switch>
+                <Route path='/' >This is the home page</Route>
+                <Route path='/join' component={RoomJoinPage}></Route>
+                <Route path='/join' component={CreateRoomPage}></Route>
+            </Switch>
+        </Router>
+    }
+}
