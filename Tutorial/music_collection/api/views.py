f9d13032617cccd18cdafc2f3fd390fc22f391bc
kilobyte1 <p2562416@my365.dmu.ac.uk>
2024-08-28 23:18:49 +0100

    did an indept analysis on the CreateRoomView and added comments

--- Tutorial/music_collection/api/views.py
+++ Tutorial/music_collection/api/views.py
@@ -19,25 +19,36 @@ class RoomView(generics.ListCreateAPIView):
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
-
+    #When a client sends a POST request to the view, Django will automatically invoke this method
+    #self: This is a reference to the instance of the class
     def post(self, request, format=None):
+        #check if the user session already exists or not expired
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
-
+        #from the request, get the data
         serializer = self.serializer_class(data=request.data)
+        #check if the data contains the serialised data required from the createroomseria...
         if serializer.is_valid():
+            #if calid, means the 2 data s in the request
+            #now we can get them using get method
             guests_can_pause =serializer.data.get("guests_can_pause")
             votes_to_skip = serializer.data.get("votes_to_skip")
+            #we get the host "Id" from the session
             host=self.request.session.session_key
+            #now we get all the existing rooms created by the host
             queryset = Room.objects.filter(host=host)
+            #if the host(the session or the user) has an account or active session
             if queryset.exists():
+                #get the first room(most deffo will be 1 anyways)
                 room = queryset[0]
+                #now assign the data from the serialiser(request) to vroom details if the room exist
                 room.guests_can_pause = guests_can_pause
                 room.votes_to_skip = votes_to_skip
+                #we want to update some fields
                 room.save(update_fields=['guests_can_pause', 'votes_to_skip'])
             else:
+                #if the queryset ie empty(no room with that sessio) we create a new room
                 room =Room(host=host, guests_can_pause = guests_can_pause, votes_to_skip = votes_to_skip)
                 room.save()
-
             return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
         
