2b14ce509e2988620fd259cb8f7a352047359fea
kilobyte1 <p2562416@my365.dmu.ac.uk>
2024-08-31 13:07:16 +0100

    Sent a post request from the frontend and log the response

--- Tutorial/music_collection/frontend/src/components/CreateRoomPage.js
+++ Tutorial/music_collection/frontend/src/components/CreateRoomPage.js
@@ -11,27 +11,47 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
-  defaltVotes = 2;
+  defaultVotes = 2;
   constructor(props) {
     super(props);
-    this.tsate = {
+    this.state = {
       geustCanPause: true,
-      voteToSkip: this.defaltVotes,
+      voteToSkip: this.defaultVotes,
     };
+    //bind the method to the class so it has access  to the this keyword
     this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+    this.handleVotesChange = this.handleVotesChange.bind(this);
+    this.handleGuestCanPauChange = this.handleGuestCanPauChange.bind(this);
   }
 
+  //get the value of the text field
   handleVotesChange(e) {
     this.setState({ voteToSkip: e.target.value });
   }
 
+  //
   handleGuestCanPauChange(e) {
     this.setState({
+      //if the value of guest can pause if true, then true else false
       geustCanPause: e.target.value === "true" ? true : false,
     });
   }
   handleRoomButtonPressed() {
-    console.log(this.state);
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        votes_to_skip: this.state.voteToSkip,
+        guests_can_pause: this.state.geustCanPause,
+      }),
+    };
+    //send the requestOptions created at the top - to the endpoint
+    //then once we get a response, convert to json
+    //then log the data to console
+    fetch("/api/create-room", requestOptions)
+      .then((response) => response.json())
+      //data from this.response.json
+      .then((data) => console.log(data));
   }
 
   render() {
